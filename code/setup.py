648e1aa081c62e4ad04f67171a58b8838973bae8
tt-213268 <takalkartejastt@gmail.com>
2024-02-13 09:08:02 +0100

    trying out new functions

--- code/setup.py
+++ code/setup.py
@@ -1,4 +1,4 @@
-import pybullet as p
+import pybullet as pybullet
 import pybullet_data
 import os
 import time
@@ -7,36 +7,63 @@ import time
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 # Start the simulation
-physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath())  # Set the search path to find URDF files
+physicsClient = pybullet.connect(pybullet.GUI)  # or pybullet.DIRECT for non-graphical version
+pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())  # Set the search path to find URDF files
 
 # Load the ground plane
-planeId = p.loadURDF("plane.urdf")
+planeId = pybullet.loadURDF("plane.urdf")
 
 # Load your robot URDF file
 robotStartPos = [0, 0, 0]  # Coordinates of the robot in the world
-robotStartOrientation = p.getQuaternionFromEuler([0, 0, 0])  # Robot's orientation (roll, pitch, yaw)
+robotStartOrientation = pybullet.getQuaternionFromEuler([0, 0, 0])  # Robot's orientation (roll, pitch, yaw)
 
 # Construct the full path to the URDF file
 
 robot_urdf_file = os.path.join(script_dir, "urdf/ur5.urdf")
-robotId = p.loadURDF(robot_urdf_file, robotStartPos, robotStartOrientation)
+robot = pybullet.loadURDF(robot_urdf_file, robotStartPos, robotStartOrientation, useFixedBase=1)
 
-# Simulation loop
-p.setGravity(0, 0, -9.8)  # Set gravity
-p.setTimeStep(1.0 / 240.0)  # Set the simulation time step
-print ('before steps')
 
-# Run the simulation for 30 seconds
-simulation_time = 30  # in seconds
-start_time = time.time()
 
-while (time.time() - start_time) < simulation_time:
-    p.stepSimulation()
+# # Run the simulation for 30 seconds
+# simulation_time = 1  # in seconds
+# start_time = time.time()
+
+# while (time.time() - start_time) < simulation_time:
+#     pybullet.stepSimulation()
     
 # # Run the simulation for 1000 steps (adjust as needed)
 # for i in range(1000):
-#     p.stepSimulation()
-print ('after steps')
+#     pybullet.stepSimulation()
+
+
+
+
+
+'''simulation'''
+pybullet.setGravity(0, 0, -9.81)  # Set gravity
+pybullet.setTimeStep(0.0001)  # Set the simulation time step
+pybullet.setRealTimeSimulation(0)
+pybullet.setJointMotorControlArray(
+    robot, range(6), pybullet.POSITION_CONTROL,
+    targetPositions=[0.9] * 6)
+for _ in range(100000):
+    pybullet.stepSimulation()
+
+'''get parameters'''
+position, orientation = pybullet.getBasePositionAndOrientation(robot)
+orientation = pybullet.getEulerFromQuaternion(orientation)
+NumberOfjoints = pybullet.getNumJoints(robot)
+
+joint_index = 2
+joint_info = pybullet.getJointInfo(robot, joint_index)
+name, joint_type, lower_limit, upper_limit = \
+    joint_info[1], joint_info[2], joint_info[8], joint_info[9]
+
+joint_positions = [j[0] for j in pybullet.getJointStates(robot, range(6))]
+joint_positions
+
+print ('before steps')
+
 # End the simulation
-p.disconnect()
+pybullet.disconnect()
+print (joint_positions)
