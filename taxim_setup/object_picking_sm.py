6811c972219e52ce52290bb251cc2dc771152854
tt-213268 <takalkartejastt@gmail.com>
2024-04-30 16:15:05 +0200

    algorithm to grasp multiple objects in sequence

--- taxim_setup/object_picking_sm.py
+++ taxim_setup/object_picking_sm.py
@@ -1,6 +1,8 @@
 import pybullet as pybullet
 import pybullet_data
 import os
+import glob
+import xml.etree.ElementTree as ET
 import time
 import math
 import pybulletX as px
@@ -19,7 +21,7 @@ from setup2 import getObjInfo
 import robot_functions
 from math import pi
 import csv
-
+from stl import mesh
 #to enumarate states and events in FSM
 from enum import Enum 
 
@@ -27,8 +29,10 @@ class setup():
 
     def __init__(self):
         self.pybullet_data_location = "/app/bullet3/examples/pybullet/gym/pybullet_data"
+        self.objects_location = '/app/Taxim/experiments/setup2/objects2'
         global Entity
-    def set_camera_to_robot(self):
+        self.object_names = [name for name in os.listdir(self.objects_location) if os.path.isdir(os.path.join(self.objects_location, name))]
+    def set_camera_to_robot(self) :
         global pybullet
         self.cdist=1 
         self.cyaw=180 
@@ -65,6 +69,66 @@ class setup():
             if keys.get(120):  #X
                 self.cdist-=.01
 
+    def get_object_list(self):
+        
+        print(self.object_names)
+        # return object_names'
+
+    def get_height(self,obj_path):
+        lcut = hcut = None
+        with open(obj_path) as file:
+            while 1:
+                line = file.readline()
+                if not line:
+                    break
+                strs = line.split(" ")
+                if strs[0] == "v":
+                    z_cor = float(strs[3])
+                    if lcut is None or z_cor < lcut:
+                        lcut = z_cor
+                    if hcut is None or z_cor > hcut:
+                        hcut = z_cor
+                if strs[0] == "vt":
+                    break
+        return hcut - lcut
+
+    def getObjInfo(self,objName):
+        assert objName in self.object_names
+        urdf_path = os.path.join("/app/Taxim/experiments/setup2/objects2", objName, "model.urdf")
+        tree = ET.parse(urdf_path)
+        mass_node = next(tree.iter('mass'))
+        if mass_node is None:
+            raise KeyError("No mass in the urdf file.")
+        mass = float(mass_node.attrib["value"])
+
+        friction_node = next(tree.iter('lateral_friction'))
+        if friction_node is None:
+            raise KeyError("No friction in the urdf file.")
+        friction = float(friction_node.attrib["value"])
+
+        obj_mesh_list = glob.glob(os.path.join(self.objects_location, objName, "*.obj"))
+        if len(obj_mesh_list) > 0:
+            obj_path = obj_mesh_list[0]
+            height = self.get_height(obj_path)
+        else:
+            mesh_file_name = os.path.join(self.objects_location, objName, "*.stl")
+            mesh_file_list = glob.glob(mesh_file_name)
+            stl_path = mesh_file_list[0]
+            stl_file = mesh.Mesh.from_file(stl_path)
+            height = np.max(stl_file.z) - np.min(stl_file.z)
+
+        force_range = np.array(
+            [0.013903, 0.08583400000000001, 0.18635599999999997, 0.301228, 0.44313, 0.6062639999999999,
+            0.7980979999999996,
+            1.006655, 1.255222, 1.498395, 1.791708, 2.10153, 2.4639089999999997, 2.873739, 3.3301070000000004,
+            3.8420690000000004, 4.392766999999999, 4.958345, 5.535276, 6.171562, 6.803239000000002,
+            7.445841000000001,
+            8.154558, 8.841395, 9.530221, 10.181313, 10.924032999999998, 11.770725, 12.293285000000001,
+            13.091981999999998])
+        deformation = np.arange(0.0001, 0.0031, 0.0001)
+
+        return urdf_path, mass, height, force_range, deformation, friction
+
 
 class control():
     def __init__(self):
@@ -100,6 +164,23 @@ class entities():
         self.robot_urdf_file = os.path.join(script_dir, "urdf/ur5_robotiq_85.urdf")
         self.robot = pybullet.loadURDF(self.robot_urdf_file, self.robotStartPos, self.robotStartOrientation, useFixedBase=1)
         self.rob = Robot(self.robot)
+    def set_joints_friction(self,joint_friction):
+                    # Set friction for each joint
+        num_joints = 6
+        # Set friction for each joint
+        for joint_index in range(num_joints):
+            # Get joint information
+            joint_info = pybullet.getJointInfo(self.robot, joint_index)
+            joint_name = joint_info[1].decode("utf-8")
+
+            # Check if joint is revolute or prismatic
+            if joint_info[2] == pybullet.JOINT_REVOLUTE or joint_info[2] == pybullet.JOINT_PRISMATIC:
+                # Set friction
+                pybullet.changeDynamics(self.robot, joint_index, lateralFriction=joint_friction)  # Adjust friction value as needed
+
+# Run simulation
+
+   
     def initialize_robot_at(self,initial_joint_positions):
         num_joints = 6
         for joint_index in range(num_joints):
@@ -109,9 +190,13 @@ class entities():
             # Load the ground plane
         self.planeId = pybullet.loadURDF("plane.urdf")
 
-    def load_object(self):
+    def load_object(self,obj_select_id):
         # self.urdfObj, self.obj_mass, self.obj_height, self.force_range, self.deformation, _ = getObjInfo("RubiksCube")
-        self.urdfObj, self.obj_mass, self.obj_height, self.force_range, self.deformation, _ = getObjInfo("TomatoSoupCan")
+        #['YcbPottedMeatCan', 'YcbStrawberry','YcbTomatoSoupCan', 
+        # 'YcbMustardBottle', 'YcbChipsCan', 'YcbFoamBrick', 'YcbPear', 'MustardBottle', 'TomatoSoupCan', 
+        # 'RubiksCube', 'YcbTennisBall', 'YcbMediumClamp']
+        self.urdfObj, self.obj_mass, self.obj_height, self.force_range, self.deformation, _ = Setup.getObjInfo(Setup.object_names[obj_select_id])
+        # self.urdfObj, self.obj_mass, self.obj_height, self.force_range, self.deformation, _ = Setup.getObjInfo("TomatoSoupCan")
         self.objStartPos = [0.09, 0.35, self.obj_height / 2 ]
         self.objStartOrientation = pybullet.getQuaternionFromEuler([0, 0, np.pi / 2])
         self.objID = pybullet.loadURDF(self.urdfObj, self.objStartPos, self.objStartOrientation)
@@ -127,6 +212,7 @@ class entities():
         ]
         self.gripperJoints = Entity.rob.get_id_by_name(self.gripperNames)
 
+
 class slip_simulation():
     def __init__(self):
         self.x =0
@@ -135,7 +221,13 @@ class slip_simulation():
         self.obj_position_z_array = []
         self.sum =0
         self.new_mass = 0.1
-
+        self.object_fell = False
+    def reset_variables(self):
+        self.create_slip_entry_point = True
+        self.obj_position_z_array = []
+        self.sum =0
+        self.new_mass = 0.1
+        self.object_fell = False       
     def init_ss(self):
         #start the robot in this position
         self.initial_joint_positions = [-math.pi/2,-math.pi/2,-math.pi/2,math.pi/2,-math.pi/2,-math.pi/2] 
@@ -226,9 +318,20 @@ class slip_simulation():
         if t >self.entry_time+ 50:
             if t%3==0:
                 pos_diff_z = self.avg_obj_position_z - obj_position_z
-                print(pos_diff_z)
-                if pos_diff_z < 0.01:
+                # print(pos_diff_z)
+                if pos_diff_z < 0.02:
+                    self.slip_occured =False
                     self.new_mass = self.new_mass + 0.1
+                else:
+                    self.slip_occured = True
+    def check_object_fall(self):
+        
+        obj_position,obj_orientation = pybullet.getBasePositionAndOrientation(Entity.objID)
+        obj_position_z = obj_position[2] 
+        if abs(obj_position_z - self.avg_obj_position_z) > 0.2:
+            self.object_fell = True
+        else:
+            self.object_fell = False
     
     def reset_grasp(self,gripper_positions):
         diff = abs(gripper_positions[0] -  gripper_positions[1])
@@ -328,6 +431,8 @@ Control = control()
 SS = slip_simulation()
 # Get the directory of the Python file
 script_dir = os.path.dirname(os.path.realpath(__file__))
+print('script dir=',script_dir)
+
 
 
 # Call the main function if this script is executed directly
@@ -336,11 +441,10 @@ if __name__ == "__main__":
     Setup.start_simulation()
     Entity.loadPlane()
     Entity.loadRobot(script_dir)
-    Entity.load_object()
-    Entity.rob.gripper_open()
-    Entity.init_gripper()
-    Setup.set_camera_to_robot()
+    # Entity.set_joints_friction(1000)
 
+    Setup.set_camera_to_robot()
+    obj_select_id = 0
     #start slip simulation by moving robot to object
     SS.init_ss()
     
@@ -351,6 +455,8 @@ if __name__ == "__main__":
     slip_counter = 0
     after_picking_up = False
     #try and execept are used for keyboard interrupt cntrl+c
+
+    Setup.get_object_list()
     try:
         
         t=0
@@ -360,6 +466,11 @@ if __name__ == "__main__":
             pybullet.stepSimulation()
             Setup.adjust_camera_with_keyboard()            
             Control.dynamic_delay()
+            if t ==1:
+                print('start')
+                Entity.rob.gripper_open()
+                Entity.init_gripper()
+                Entity.load_object(obj_select_id)
 
             if t == 100:
                 SS.go_to_object()
@@ -376,16 +487,29 @@ if __name__ == "__main__":
             #after robot picked up object and went back
             if t> 600 and SS.robot_stopped()==True:
                 after_picking_up = True
-                
+
             if after_picking_up == True:
                 if reset_grasp==True:
                     gripper_positions = [pybullet.getJointState(Entity.robot, joint_index)[0] for joint_index in Entity.gripperJoints]
+                    print('reset grasp')
                     SS.reset_grasp(gripper_positions)
                     reset_grasp=False
                 if slip_counter > 50:
                     SS.create_slip(t)
                 slip_counter+=1     
-                
+            
+            if t>1000 and SS.object_fell==False:
+
+                if t%3 ==0:
+                    SS.check_object_fall()
+                    if SS.object_fell ==True:
+                        pybullet.removeBody(Entity.objID)
+                        t=0
+                        obj_select_id = obj_select_id + 1
+                        after_picking_up = False
+                        reset_grasp = True
+                        SS.reset_variables()
+
             t=t+1
             
     except KeyboardInterrupt:
